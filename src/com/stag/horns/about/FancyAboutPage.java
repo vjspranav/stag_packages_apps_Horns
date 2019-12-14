960d63e40c1e7c5e9ace1cd2e23eac7474e7753a
xayron <zxyron0@gmail.com>
2019-12-14 21:15:57 +0530

    Add myself and layout changes

--- src/com/stag/horns/about/FancyAboutPage.java
+++ src/com/stag/horns/about/FancyAboutPage.java
@@ -30,45 +30,51 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.stag.horns.R;
 import com.stag.horns.about.DiagonalView;
 
 import com.android.settings.R;
 
 public class FancyAboutPage extends RelativeLayout {
-    private TextView l1,l2,phenomname,vjsname,pritishname,shekhawatname,abhimanyuname,vinothname,phenomdescription,vjsdescription,pritishdescription,shekhawatdescription,abhimanyudescription,vinothdescription;
+    private TextView l1,l2,phenomname,vjsname,pritishname,rakeshname,abhimanyuname,vinothname,debjitname,phenomdescription,vjsdescription,pritishdescription,rakeshdescription,abhimanyudescription,vinothdescription,debjitdescription;
     DiagonalView diagonalView;
-    ImageView phenom,vjs,pritish,shekhawat,abhimanyu,vinoth,gg,tl,tw,git;
+    CircularImageView phenom,vjs,pritish,rakesh,abhimanyu,vinoth,debjit;
+    ImageView gg,tl,tw,git;
     String twitterurl,ggurl,tlurl,githuburl;
     private void init(Context context) {
         //do stuff that was in your original constructor...
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(
                 Context.LAYOUT_INFLATER_SERVICE);
+        assert inflater != null;
         inflater.inflate(R.layout.activity_fancy_about_page, this, true);
-        l1=(TextView) findViewById(R.id.name);
-        l2=(TextView) findViewById(R.id.description);
-        phenomname=(TextView) findViewById(R.id.phenomname);
-        vjsname=(TextView) findViewById(R.id.vjsname);
-        pritishname=(TextView) findViewById(R.id.pritishname);
-        shekhawatname=(TextView) findViewById(R.id.shekhawatname);
-        abhimanyuname=(TextView) findViewById(R.id.abhimanyuname);
-        vinothname=(TextView) findViewById(R.id.vinothname);
-        phenomdescription=(TextView) findViewById(R.id.phenomdescription);
-        vjsdescription=(TextView) findViewById(R.id.vjsdescription);
-        pritishdescription=(TextView) findViewById(R.id.pritishdescription);
-        shekhawatdescription=(TextView) findViewById(R.id.shekhawatdescription);
-        abhimanyudescription=(TextView) findViewById(R.id.abhimanyudescription);
-        vinothdescription=(TextView) findViewById(R.id.vinothdescription);
-        phenom=(ImageView) findViewById(R.id.phenom);
-        vjs=(ImageView) findViewById(R.id.vjs);
-        pritish=(ImageView) findViewById(R.id.pritish);
-        shekhawat=(ImageView) findViewById(R.id.shekhawat);
-        abhimanyu=(ImageView) findViewById(R.id.abhimanyu);
-        vinoth=(ImageView) findViewById(R.id.vinoth);
-        tw=(ImageView) findViewById(R.id.twitter);
-        gg=(ImageView) findViewById(R.id.google);
-        tl=(ImageView) findViewById(R.id.telegram);
-        git=(ImageView) findViewById(R.id.github);
-        diagonalView = (DiagonalView) findViewById(R.id.background);
+        l1= findViewById(R.id.name);
+        l2= findViewById(R.id.description);
+        phenomname= findViewById(R.id.phenomname);
+        vjsname= findViewById(R.id.vjsname);
+        pritishname= findViewById(R.id.pritishname);
+        rakeshname= findViewById(R.id.shekhawatname);
+        abhimanyuname= findViewById(R.id.abhimanyuname);
+        vinothname= findViewById(R.id.vinothname);
+        debjitname= findViewById(R.id.debjitname);
+        phenomdescription= findViewById(R.id.phenomdescription);
+        vjsdescription= findViewById(R.id.vjsdescription);
+        pritishdescription= findViewById(R.id.pritishdescription);
+        rakeshdescription= findViewById(R.id.shekhawatdescription);
+        abhimanyudescription= findViewById(R.id.abhimanyudescription);
+        vinothdescription= findViewById(R.id.vinothdescription);
+        debjitdescription= findViewById(R.id.debjitdescription);
+        phenom= findViewById(R.id.phenom);
+        vjs= findViewById(R.id.vjs);
+        pritish= findViewById(R.id.pritish);
+        rakesh= findViewById(R.id.shekhawat);
+        abhimanyu = findViewById(R.id.abhimanyu);
+        vinoth= findViewById(R.id.vinoth);
+        debjit = findViewById(R.id.debjit);
+        tw= findViewById(R.id.twitter);
+        gg= findViewById(R.id.google);
+        tl= findViewById(R.id.telegram);
+        git= findViewById(R.id.github);
+        diagonalView = findViewById(R.id.background);
     }
     public FancyAboutPage(Context context) {
         super(context);
@@ -156,25 +162,28 @@ public class FancyAboutPage extends RelativeLayout {
        phenom.setImageResource(Icon);
        vjs.setImageResource(Icon);
        pritish.setImageResource(Icon);
-       shekhawat.setImageResource(Icon);
+       rakesh.setImageResource(Icon);
        abhimanyu.setImageResource(Icon);
        vinoth.setImageResource(Icon);
+       debjit.setImageResource(Icon);
     }
     public void setAppName(String AppName){
        phenomname.setText(AppName);
        vjsname.setText(AppName);
        pritishname.setText(AppName);
-       shekhawatname.setText(AppName);
+       rakeshname.setText(AppName);
        abhimanyuname.setText(AppName);
        vinothname.setText(AppName);
+       debjitname.setText(AppName);
     }
     public void setAppDescription(String AppDescription){
         phenomdescription.setText(AppDescription);
         vjsdescription.setText(AppDescription);
         pritishdescription.setText(AppDescription);
-        shekhawatdescription.setText(AppDescription);
+        rakeshdescription.setText(AppDescription);
         abhimanyudescription.setText(AppDescription);
         vinothdescription.setText(AppDescription);
+        debjitdescription.setText(AppDescription);
     }
 
 }
